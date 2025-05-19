e4338a6754dd3ade444433fd907060b21374c4c6
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2025-05-18 21:41:42 -0700

    Fix: Fixed a container issue with the header component

--- components/ui/HeaderComponent.js
+++ components/ui/HeaderComponent.js
@@ -5,15 +5,20 @@ import Link from "next/link";
 const HeaderComponent = ({ pageName, pageLink }) => {
   return (
     <div className={styles.settingHeader}>
-      <Link href={pageLink}>
-        <Image
-          src="/icons/CaretLeft.svg"
-          alt="arrow-left"
-          width={32}
-          height={32}
-        />
-      </Link>
-      <h4>{pageName}</h4>
+      <div className={styles.left}>
+        <Link href={pageLink}>
+          <Image
+            src="/icons/CaretLeft.svg"
+            alt="arrow-left"
+            width={32}
+            height={32}
+          />
+        </Link>
+      </div>
+      <div className={styles.center}>
+        <h4>{pageName}</h4>
+      </div>
+      <div className={styles.right}></div>
     </div>
   );
 };
