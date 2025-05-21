8c0253a4d5526eb2fd66eba1ed32c594fdfdbb3e
Crite-Spranberries <113459843+Crite-Spranberries@users.noreply.github.com>
2025-05-20 20:09:20 -0700

    Add: sleep tracking main page contents! Component for the main sleep time ring that updates based on local time. The buttons and icons are image placeholders. Text is real.

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import Calendar from "@/components/feature/Calendar";
 import ThreeButton from "@/components/ui/ThreeButton";
 import { useRouter } from "next/navigation";
 import styles from "@/styles/SupplementPage.module.css";
+import TimeRing from "@/components/feature/TimeRing";
+import "@/styles/globals.css";
 
 export default function AchievementPage() {
   const router = useRouter();
@@ -21,7 +23,52 @@ export default function AchievementPage() {
           paddingBottom: 100,
           height: "100%",
         }}
-      ></main>
+      >
+        {/* Greeting */}
+        <div className="greeting">
+          <span>
+            Hello, <b>Jane</b>!
+          </span>
+          <br />
+          <span className="greetingSub">
+            Did you sleep well?{" "}
+            <span role="img" aria-label="sleep">
+              😴
+            </span>
+          </span>
+        </div>
+        {/* TimeRing and Progress Icons */}
+        <div className="centerRow">
+          <TimeRing />
+          <div className="progressIcons">
+            <Image
+              src="/images/progressicons.png"
+              alt="Progress Icons"
+              width={80}
+              height={300}
+              style={{ width: "180%", height: "auto" }}
+            />
+          </div>
+        </div>
+        {/* Sleep Now Button */}
+        <button className="sleepNowButton">Sleep now</button>
+        {/* Sleep Streak Rings */}
+        <div className="streakRings" style={{ width: 350 }}>
+          <Image
+            src="/images/sleepstreakrings.png"
+            alt="Sleep Streak Rings"
+            width={350}
+            height={70}
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+        {/* Improvement Message */}
+        <div className="improvementMsg">
+          Your sleep time has <span className="sparkle">✨improved✨</span>
+          <br />
+          Keep it up! Don't break the green streak!
+        </div>
+      </main>
 
       <nav className={styles.navigationBarGlobal}>
         <NavigationBar
