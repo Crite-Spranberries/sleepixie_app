faefc2baf64046507ea68f2782ab4cd5fe45f2cf
j1s00jang <jjang87@my.bcit.ca>
2025-05-20 22:43:54 -0700

    Profile screen in the app folder

--- .history/ui/buttons/BlueButton_20250512094311.js
+++
@@ -1,6 +0,0 @@
-import styles from "@/ui/buttons/BlueButton.module.css";
-
-const BlueButton = ({ label }) => {
-    return <button className={styles.buttonContainer}>{label}</button>;
-};
-export default BlueButton;
